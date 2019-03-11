098952e4c0a3d9c85a5d0992251c5233b4abcb62
Dean <dean.juricic@gmail.com>
2019-03-11 08:41:43 +0100

    Added thymeleaf template for index page, and mapped controller method. Closes #1.

---
+++ src/main/java/hr/scorpiusmobile/recipes/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package hr.scorpiusmobile.recipes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String showIndexPage(){
+        return "index";
+    }
+}
