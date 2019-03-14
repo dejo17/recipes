3ebd31faee3ce6a6fd1259ceb4aa5cf9ec1f7b53
Dean <dean.juricic@gmail.com>
2019-03-14 10:25:14 +0100

    Added classes Category and Dificulty to domain package. Defined OneToMany and ManyToMany (with join table) relationship to Recipe.

---
+++ src/main/java/hr/scorpiusmobile/recipes/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package hr.scorpiusmobile.recipes.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/hr/scorpiusmobile/recipes/domain/Recipe.java
+++ src/main/java/hr/scorpiusmobile/recipes/domain/Recipe.java
@@ -17,18 +17,24 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty diffculty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty diffculty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
-
 
     @Lob
     private Byte[] image;
 
     @OneToOne(cascade= CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name="recipe_category",
+            joinColumns = @JoinColumn(name= "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name= "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -116,5 +122,21 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDiffculty() {
+        return diffculty;
+    }
+
+    public void setDiffculty(Difficulty diffculty) {
+        this.diffculty = diffculty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
